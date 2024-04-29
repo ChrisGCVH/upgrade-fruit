52fa56e777bc42c6b228c7345180c5ed2560189d
Chris Cocker <Chris.Cocker@vitalhub.com>
2024-04-29 11:15:52 +0100

    Add HicomGrid taghelper

---
+++ Web/TagHelpers/HicomGridTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace HicomInterview.Web.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "hicom-grid")]
+    public class HicomConditionalDisplayTagHelper : TagHelper
+    {
+        [HtmlAttributeName("hicom-grid")]
+        public bool IsGrid { get; set; }
+
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await base.ProcessAsync(context, output);
+
+            if (IsGrid)
+            {
+                var id = output.Attributes["id"]?.Value.ToString();
+
+                var contentBuilder = new HtmlContentBuilder();
+
+                var renderGrid = new TagBuilder("div")
+                {
+                    TagRenderMode = TagRenderMode.Normal
+                };
+
+                renderGrid.Attributes["id"] = $"{id}Rendered";
+                contentBuilder.AppendHtml(renderGrid);
+
+                var script = new TagBuilder("script")
+                {
+                    TagRenderMode = TagRenderMode.Normal
+                };
+
+                script.InnerHtml.AppendHtml($@"
+                    new HicomGrid('{id}', '{id}Rendered');
+                ");
+                contentBuilder.AppendHtml(script);
+
+
+                output.PostElement.SetHtmlContent(contentBuilder);
+            }
+        }
+    }
+}
